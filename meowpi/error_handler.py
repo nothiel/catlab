9d6b3784b2cb8418de3d3abfd1eeb098562c7021
Nothiel <rafaellb2555@gmail.com>
2022-04-13 21:59:53 -0300

    feat: add POST route and create error_handler
    On this commit we added a temporary handler to the POST method, and we
    added a new file, containing exception handler to all mapped errors.

---
+++ meowpi/error_handler.py
@@ -0,0 +1,22 @@
+from fastapi import Request
+from fastapi.responses import JSONResponse
+from meowpi.utils.errors import UserAlreadyCreated, UserNotFoundError
+
+
+
+
+def setup_exception_handlers(app):
+
+    @app.exception_handler(UserNotFoundError)
+    async def not_found_exception_handler(request: Request, exc: UserNotFoundError):
+        return JSONResponse(
+            status_code=404,
+            content={"message": f"User not found"},
+        )
+
+    @app.exception_handler(UserAlreadyCreated)
+    async def alread_created_exc_handler(request: Request, exc: UserAlreadyCreated):
+        return JSONResponse(
+            status_code=400,
+            content={"message": f"Username is already being used"}
+        )
--- meowpi/main.py
+++ meowpi/main.py
@@ -1,18 +1,11 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import JSONResponse
+from fastapi import FastAPI
 from meowpi.database.mongo import mongo_database
 from meowpi.models.users import CreateUserSchema
-from meowpi.utils.errors import UserNotFoundError
-
+from meowpi.error_handler import setup_exception_handlers
 
 app = FastAPI()
 
-@app.exception_handler(UserNotFoundError)
-async def not_found_exception_handler(request: Request, exc: UserNotFoundError):
-    return JSONResponse(
-        status_code=404,
-        content={"message": f"User not found"},
-    )
+
 
 @app.get('/')
 async def meow_world():
@@ -25,4 +18,7 @@ async def get_user(username: str):
 
 @app.post('/user')
 async def create_user(create_user_data: CreateUserSchema):
-    return "aoba"
+    user_id = await mongo_database.create_user(create_user_data)
+    return user_id
+
+setup_exception_handlers(app)
